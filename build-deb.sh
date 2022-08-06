3f23f76030f30194bd7fe3ca7a5324a7f59d2224
kokardy <mgc1028@gmail.com>
2022-08-06 17:14:55 +0900

    change image name

--- build-deb.sh
+++ build-deb.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
 # build docker image
-docker build $1 -t $USER/mozc .
+docker build $1 -t kokardy/mozc .
 
 # run container
-docker run -d --name mozc $USER/mozc tail -f /dev/null
+docker run -d --name mozc kokardy/mozc tail -f /dev/null
 
 # copy *.deb
 docker cp mozc:/home/mozc_builder/work/deb ./
